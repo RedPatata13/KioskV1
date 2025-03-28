a44f32ad110771e60b51e7fb95fcd8ebe6767f41
Xpnsive03 <marynelle.aban1997@gmail.com>
2025-03-29 01:18:53 +0800

    Created a BaseRepository

---
+++ Repositories/SuppliersRepository.vb
@@ -0,0 +1,23 @@
+﻿Public Class SuppliersRepository
+    Implements IBaseRepository(Of Supplier)
+
+    Public Sub Add(entity As Supplier) Implements IBaseRepository(Of Supplier).Add
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub Update(entity As Supplier) Implements IBaseRepository(Of Supplier).Update
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub Delete(id As Integer) Implements IBaseRepository(Of Supplier).Delete
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Function GetAll() As IEnumerable(Of Supplier) Implements IBaseRepository(Of Supplier).GetAll
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function GetById(id As Integer) As Supplier Implements IBaseRepository(Of Supplier).GetById
+        Throw New NotImplementedException()
+    End Function
+End Class
--- Repositories/UsersRepository.vb
+++ Repositories/UsersRepository.vb
@@ -1,37 +1,8 @@
 ﻿Imports System.Data.Entity
 
 Public Class UsersRepository
-    Implements IBaseRepository(Of User)
-
-    Private ReadOnly _context As AppDbContext
-
+    Inherits BaseRepository(Of User)
     Public Sub New(context As AppDbContext)
-        _context = context
+        MyBase.New(context)
     End Sub
-
-    Public Sub Add(entity As User) Implements IBaseRepository(Of User).Add
-        _context.Users.Add(entity)
-        ' No SaveChanges() here!
-    End Sub
-
-    Public Sub Update(entity As User) Implements IBaseRepository(Of User).Update
-        _context.Entry(entity).State = EntityState.Modified
-        ' No SaveChanges() here!
-    End Sub
-
-    Public Sub Delete(id As Integer) Implements IBaseRepository(Of User).Delete
-        Dim user = _context.Users.Find(id)
-        If user IsNot Nothing Then
-            _context.Users.Remove(user)
-        End If
-        ' No SaveChanges() here!
-    End Sub
-
-    Public Function GetAll() As IEnumerable(Of User) Implements IBaseRepository(Of User).GetAll
-        Return _context.Users.AsNoTracking().ToList()
-    End Function
-
-    Public Function GetById(id As Integer) As User Implements IBaseRepository(Of User).GetById
-        Return _context.Users.Find(id)
-    End Function
 End Class
