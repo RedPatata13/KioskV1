ffe8432f6f85721f5682f8026d6c4db2934f54cc
Red Alien <redpatatas13@gmail.com>
2025-04-04 08:24:06 +0800

    added landing page

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -83,6 +83,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property icons8_hamburger_96() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("icons8-hamburger-96", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property icons8_supply_chain_100() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("icons8-supply-chain-100", resourceCulture)
