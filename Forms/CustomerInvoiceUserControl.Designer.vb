b5b8c669d28714ca745827c8824375d6466f3c9c
Sandra Agustin <Agustinsandy03@gmail.com>
2025-04-17 17:25:18 +0800

    printable order num

--- Forms/CustomerInvoiceUserControl.Designer.vb
+++ Forms/CustomerInvoiceUserControl.Designer.vb
@@ -23,41 +23,43 @@
         'Do not modify it using the code editor.
         <System.Diagnostics.DebuggerStepThrough()>
         Private Sub InitializeComponent()
-            Me.OrderNumberLabel = New Guna.UI2.WinForms.Guna2HtmlLabel()
+            Me.OrderNumber = New Guna.UI2.WinForms.Guna2HtmlLabel()
             Me.OrderPanel = New System.Windows.Forms.Panel()
             Me.OrderNoLbl = New Guna.UI2.WinForms.Guna2HtmlLabel()
             Me.YourOrderLbl = New Guna.UI2.WinForms.Guna2HtmlLabel()
             Me.CustomerReceiptLbl2 = New Guna.UI2.WinForms.Guna2HtmlLabel()
             Me.CustomerReceiptLbl = New Guna.UI2.WinForms.Guna2HtmlLabel()
+            Me.PrintDoc = New System.Drawing.Printing.PrintDocument()
+            Me.PrintOrderNumberButton = New Guna.UI2.WinForms.Guna2Button()
             Me.OrderPanel.SuspendLayout()
             Me.SuspendLayout()
             '
-            'OrderNumberLabel
+            'OrderNumber
             '
-            Me.OrderNumberLabel.BackColor = System.Drawing.Color.Transparent
-            Me.OrderNumberLabel.Font = New System.Drawing.Font("Poppins", 120.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-            Me.OrderNumberLabel.Location = New System.Drawing.Point(137, 60)
-            Me.OrderNumberLabel.Name = "OrderNumberLabel"
-            Me.OrderNumberLabel.Size = New System.Drawing.Size(160, 284)
-            Me.OrderNumberLabel.TabIndex = 2
-            Me.OrderNumberLabel.Text = "31"
+            Me.OrderNumber.BackColor = System.Drawing.Color.Transparent
+            Me.OrderNumber.Font = New System.Drawing.Font("Poppins", 120.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+            Me.OrderNumber.Location = New System.Drawing.Point(101, 52)
+            Me.OrderNumber.Name = "OrderNumber"
+            Me.OrderNumber.Size = New System.Drawing.Size(211, 284)
+            Me.OrderNumber.TabIndex = 2
+            Me.OrderNumber.Text = "00"
             '
             'OrderPanel
             '
             Me.OrderPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-            Me.OrderPanel.Controls.Add(Me.OrderNumberLabel)
+            Me.OrderPanel.Controls.Add(Me.OrderNumber)
             Me.OrderPanel.Controls.Add(Me.OrderNoLbl)
             Me.OrderPanel.Controls.Add(Me.YourOrderLbl)
-            Me.OrderPanel.Location = New System.Drawing.Point(499, 337)
+            Me.OrderPanel.Location = New System.Drawing.Point(725, 444)
             Me.OrderPanel.Name = "OrderPanel"
-            Me.OrderPanel.Size = New System.Drawing.Size(381, 288)
+            Me.OrderPanel.Size = New System.Drawing.Size(388, 288)
             Me.OrderPanel.TabIndex = 5
             '
             'OrderNoLbl
             '
             Me.OrderNoLbl.BackColor = System.Drawing.Color.Transparent
             Me.OrderNoLbl.Font = New System.Drawing.Font("Poppins", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-            Me.OrderNoLbl.Location = New System.Drawing.Point(80, 207)
+            Me.OrderNoLbl.Location = New System.Drawing.Point(44, 198)
             Me.OrderNoLbl.Name = "OrderNoLbl"
             Me.OrderNoLbl.Size = New System.Drawing.Size(51, 53)
             Me.OrderNoLbl.TabIndex = 1
@@ -67,7 +69,7 @@
             '
             Me.YourOrderLbl.BackColor = System.Drawing.Color.Transparent
             Me.YourOrderLbl.Font = New System.Drawing.Font("Poppins", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-            Me.YourOrderLbl.Location = New System.Drawing.Point(107, 26)
+            Me.YourOrderLbl.Location = New System.Drawing.Point(118, 18)
             Me.YourOrderLbl.Name = "YourOrderLbl"
             Me.YourOrderLbl.Size = New System.Drawing.Size(163, 53)
             Me.YourOrderLbl.TabIndex = 0
@@ -77,7 +79,7 @@
             '
             Me.CustomerReceiptLbl2.BackColor = System.Drawing.Color.Transparent
             Me.CustomerReceiptLbl2.Font = New System.Drawing.Font("Poppins SemiBold", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-            Me.CustomerReceiptLbl2.Location = New System.Drawing.Point(474, 153)
+            Me.CustomerReceiptLbl2.Location = New System.Drawing.Point(713, 235)
             Me.CustomerReceiptLbl2.Name = "CustomerReceiptLbl2"
             Me.CustomerReceiptLbl2.Size = New System.Drawing.Size(429, 86)
             Me.CustomerReceiptLbl2.TabIndex = 4
@@ -87,22 +89,45 @@
             '
             Me.CustomerReceiptLbl.BackColor = System.Drawing.Color.Transparent
             Me.CustomerReceiptLbl.Font = New System.Drawing.Font("Poppins SemiBold", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-            Me.CustomerReceiptLbl.Location = New System.Drawing.Point(56, 69)
+            Me.CustomerReceiptLbl.Location = New System.Drawing.Point(295, 151)
             Me.CustomerReceiptLbl.Name = "CustomerReceiptLbl"
             Me.CustomerReceiptLbl.Size = New System.Drawing.Size(1244, 86)
             Me.CustomerReceiptLbl.TabIndex = 3
             Me.CustomerReceiptLbl.Text = "Please get your receipt and proceed to the counter "
+            '
+            'PrintDoc
+            '
+            '
+            'PrintOrderNumberButton
+            '
+            Me.PrintOrderNumberButton.BorderColor = System.Drawing.Color.LightGray
+            Me.PrintOrderNumberButton.BorderThickness = 1
+            Me.PrintOrderNumberButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+            Me.PrintOrderNumberButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+            Me.PrintOrderNumberButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+            Me.PrintOrderNumberButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+            Me.PrintOrderNumberButton.FillColor = System.Drawing.Color.Transparent
+            Me.PrintOrderNumberButton.Font = New System.Drawing.Font("Poppins", 9.0!)
+            Me.PrintOrderNumberButton.ForeColor = System.Drawing.Color.DimGray
+            Me.PrintOrderNumberButton.Location = New System.Drawing.Point(725, 738)
+            Me.PrintOrderNumberButton.Name = "PrintOrderNumberButton"
+            Me.PrintOrderNumberButton.PressedColor = System.Drawing.SystemColors.ControlDark
+            Me.PrintOrderNumberButton.Size = New System.Drawing.Size(388, 30)
+            Me.PrintOrderNumberButton.TabIndex = 6
+            Me.PrintOrderNumberButton.Text = "Print"
+            Me.PrintOrderNumberButton.TextOffset = New System.Drawing.Point(0, 2)
             '
             'CustomerInvoiceUserControl
             '
             Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.BackColor = System.Drawing.Color.White
+            Me.Controls.Add(Me.PrintOrderNumberButton)
             Me.Controls.Add(Me.OrderPanel)
             Me.Controls.Add(Me.CustomerReceiptLbl2)
             Me.Controls.Add(Me.CustomerReceiptLbl)
             Me.Name = "CustomerInvoiceUserControl"
-            Me.Size = New System.Drawing.Size(1370, 749)
+            Me.Size = New System.Drawing.Size(1920, 1080)
             Me.OrderPanel.ResumeLayout(False)
             Me.OrderPanel.PerformLayout()
             Me.ResumeLayout(False)
@@ -110,12 +135,14 @@
 
         End Sub
 
-        Friend WithEvents OrderNumberLabel As Guna.UI2.WinForms.Guna2HtmlLabel
+        Friend WithEvents OrderNumber As Guna.UI2.WinForms.Guna2HtmlLabel
         Friend WithEvents OrderPanel As Panel
         Friend WithEvents OrderNoLbl As Guna.UI2.WinForms.Guna2HtmlLabel
         Friend WithEvents YourOrderLbl As Guna.UI2.WinForms.Guna2HtmlLabel
         Friend WithEvents CustomerReceiptLbl2 As Guna.UI2.WinForms.Guna2HtmlLabel
         Friend WithEvents CustomerReceiptLbl As Guna.UI2.WinForms.Guna2HtmlLabel
+        Friend WithEvents PrintDoc As Printing.PrintDocument
+        Friend WithEvents PrintOrderNumberButton As Guna.UI2.WinForms.Guna2Button
     End Class
 
 End Namespace
--- Forms/CustomerInvoiceUserControl.vb
+++ Forms/CustomerInvoiceUserControl.vb
@@ -1,6 +1,46 @@
 ﻿Namespace KioskV0.Forms
     Public Class CustomerInvoiceUserControl
+        Private Sub CustomerInvoiceUserControl_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        End Sub
+
+        Private Sub CustomerReceiptLbl2_Click(sender As Object, e As EventArgs) Handles CustomerReceiptLbl2.Click
+
+        End Sub
+
+        Private Sub CustomerReceiptLbl_Click(sender As Object, e As EventArgs) Handles CustomerReceiptLbl.Click
+
+        End Sub
+
+        Private Sub OrderPanel_Paint(sender As Object, e As PaintEventArgs) Handles OrderPanel.Paint
+
+        End Sub
+
+        Private Sub OrderNumberLabel_Click(sender As Object, e As EventArgs) Handles OrderNumber.Click
+
+        End Sub
+
+        Private Sub PrintDoc_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDoc.PrintPage
+
+            Dim bmp As New Bitmap(OrderPanel.Width, OrderPanel.Height)
+            OrderPanel.DrawToBitmap(bmp, New Rectangle(0, 0, OrderPanel.Width, OrderPanel.Height))
+            Dim scaleFactor As Single = Math.Min(e.MarginBounds.Width / OrderPanel.Width, e.MarginBounds.Height / OrderPanel.Height)
+            e.Graphics.ScaleTransform(scaleFactor, scaleFactor)
+            e.Graphics.DrawImage(bmp, 0, 0)
+
+            Dim orderNoFont As New Font("Poppins", 22, FontStyle.Bold)
+            Dim yourOrderFont As New Font("Poppins", 22, FontStyle.Bold)
+            Dim orderNumberFont As New Font("Poppins", 120, FontStyle.Bold)
+
+            e.Graphics.DrawString(YourOrderLbl.Text, yourOrderFont, Brushes.Black, 50, 50)
+            e.Graphics.DrawString(OrderNoLbl.Text, orderNoFont, Brushes.Black, 50, 100)
+            e.Graphics.DrawString(OrderNumber.Text, orderNumberFont, Brushes.Black, 50, 150)
+
+        End Sub
+
+        Private Sub PrintOrderNumberButton_Click(sender As Object, e As EventArgs) Handles PrintOrderNumberButton.Click
+            PrintDoc.Print()
+        End Sub
     End Class
 
 End Namespace
